f5bc5d77ba81b09f3cd4e4b80a2f900dd37c722b
root <satoki.taira@g.softbank.co.jp>
2018-03-08 15:17:52 +0900

    remove height

--- index.js
+++ index.js
@@ -14,11 +14,7 @@ mf.comp.LoginForm = class extends Form {
         try {
             super();
             this.name('LoginForm');
-            if (null === po) {
-                this.adom();
-            } else {
-                this.prmOpt(po);
-            }
+            this.prmOpt(po);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -28,7 +24,6 @@ mf.comp.LoginForm = class extends Form {
     initDomConts (prm) {
         try {
             super.initDomConts(prm);
-
             this.addChild(
                 new Input({
                     label   : 'Username',
@@ -45,7 +40,7 @@ mf.comp.LoginForm = class extends Form {
                     secret  : true
                 })
             );
-            this.size(this.size());
+            
             this.submitComp().text('Login');
             
         } catch (e) {
@@ -60,67 +55,6 @@ mf.comp.LoginForm = class extends Form {
             if (null !== inp) {
                 //this.input(inp);
             }
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    size (siz) {
-        try {
-            if (undefined === siz) {
-                /* getter */
-                if (undefined === this.m_size) {
-                    this.size(20);
-                }
-                return this.m_size;
-            }
-            /* setter */
-            if ('number' !== typeof siz) {
-                throw new Error('invalid paramter');
-            }
-            this.m_size = siz;
-            
-            let chd = this.child();
-            for (let cidx in chd) {
-                if ( ((this.child().length-1) == cidx) &&
-                     (true === mf.func.isObject(chd[cidx], 'Component')) ) {
-                    this.submitComp().height(siz-(siz/3));
-                } else {
-                    chd[cidx].height(siz);
-                }
-            }
-            
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    height () {
-        try {
-            let hei = 0;
-            let buf = null;
-            let chd = this.child();
-            for (let cidx in chd) {
-                if ( (true === mf.func.isInclude(chd[cidx], 'Message')) &&
-                     (true !== chd[cidx].visible()) ) {
-                    /* noting to do */
-                    continue;
-                } else if ( ((this.child().length-1) == cidx) &&
-                            (true === mf.func.isObject(chd[cidx], 'Component')) ) {
-                    hei += this.submitComp().height();
-                } else {
-                    buf = null;
-                    buf = chd[cidx].height();
-                    if ('number' === typeof buf) {
-                        hei += buf;
-                    } 
-                }
-                hei += this.getConfig('layout', 'Margin').value();
-            }
-            hei += this.getConfig('layout', 'Margin').value();
-            return hei;
         } catch (e) {
             console.error(e.stack);
             throw e;
